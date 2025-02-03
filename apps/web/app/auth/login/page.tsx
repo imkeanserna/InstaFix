b10f9b592f8eb22901b6eaf427d2fb993e88784d
imkurosaki <keandelaserna11@gmail.com>
2025-02-03 23:53:40 +0800

    ✨ feat: Checkpoint adding post individual

--- apps/web/app/auth/login/page.tsx
+++ apps/web/app/auth/login/page.tsx
@@ -1,7 +1,5 @@
 import { LoginForm } from "@repo/ui/components/auth/loginForm";
 
-export const runtime = "edge";
-
 const Page = () => {
   return (
     <div className="flex w-full h-full items-center justify-center">
